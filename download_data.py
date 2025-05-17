14cc4dcd336ad71ce50826364a9e0e3f2bf9eadf
PedramHashemi <s.pedram.h@gmail.com>
2025-05-17 09:25:11 +0200

    updating download_data

--- download_data.py
+++ download_data.py
@@ -1,6 +1,11 @@
+import os
+import shutil
 import kagglehub
 
 # Download latest version
 path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
 
 print("Path to dataset files:", path)
+# os.makedirs("data", exist_ok=True)
+shutil.move(path, "./data")
+os.system("")
